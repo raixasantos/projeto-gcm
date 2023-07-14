5d2204d3905f09c82b2bc2a1f11488df23398cbd
Angelo Gustavo <angelogustavo157@gmail.com>
2023-07-13 23:05:35 -0300

    feat: integração com api rest

---
+++ projeto-gcm-front/src/sistemabancario/Conta.java
@@ -0,0 +1,60 @@
+package sistemabancario;
+
+public class Conta {
+    private int identificador;
+    public double saldo;
+    public int pontuacao;
+    public boolean ehPoupanca;
+
+
+    public Conta() {}
+
+    public Conta(int identificador) {
+        this.identificador = identificador;
+        this.saldo = 0.0;
+        this.pontuacao = -1;
+        this.ehPoupanca = false;
+    }
+
+    public int identificador() {
+        return this.identificador;
+    }
+
+    public double saldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double creditar(double valor) {
+        this.saldo += valor;
+        if(pontuacao >= 10)
+            pontuacao += (int)(valor/100);
+        return saldo;
+    }
+
+    public double debitar(double valor) {
+        this.saldo -= valor;
+        return saldo;
+    }
+
+    public String getDados(){
+        String tipo = "";
+        if(pontuacao > -1){
+            tipo = "Bônus";
+        }
+        else if(ehPoupanca){
+            tipo = "Poupança";
+        }
+        else{
+            tipo = "Simples";
+        }
+
+        String retorno = "Tipo: "+tipo+"\nNúmero: "+identificador+"\nSaldo: "+saldo;
+        if(pontuacao > -1)
+            retorno += "\nBônus: "+pontuacao;
+        return retorno;
+    }
+}
